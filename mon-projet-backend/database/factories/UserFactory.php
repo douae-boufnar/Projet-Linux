f8ee2e6ac6bf7330d75ce0f88321be5dac190bc1
douae-boufnar <douaeboufnar91@gmail.com>
2026-05-07 19:23:37 +0100

    Update UserFactory.php

--- mon-projet-backend/database/factories/UserFactory.php
+++ mon-projet-backend/database/factories/UserFactory.php
@@ -3,33 +3,23 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
+     *
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'nom' => $this->faker->lastName(),
-            'prenom' => $this->faker->firstName(),
+            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => Hash::make('password123'),
-            'role' => 'user',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password123'),
+            'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Créer un utilisateur admin
-     */
-    public function admin(): static
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'role' => 'admin',
-            ];
-        });
     }
 }
